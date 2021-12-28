82ffd03c03ba685969078114a56c1fe69dc53339
Jakub Robaczewski <kubarob13@gmail.com>
2021-12-28 19:10:43 +0100

    Fix

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ from random import seed
 if __name__ == '__main__':
     seed(2137)
     np.random.seed(2137)
-    algorythm = GeneticAlgorythm('../resources/busy_day.in', 1, 1)
+    algorythm = GeneticAlgorythm('resources/busy_day.in', 1, 1)
     pass
